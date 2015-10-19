abb0111520bec47d7eca4411bf9716ae50694c41
giopunt <puntilgiovanni@gmail.com>
2015-10-19 14:54:48 +0000

    adding lib

---
+++ Swipe.js
@@ -0,0 +1,62 @@
+var Swipe = function(target, options){
+    this.start = {
+        x: null,
+        y: null
+    };
+    this.target = target;
+    this.options = {
+        onSwipeLeft: options.onswipeleft || function(){}, 
+        onSwipeRight: options.onswiperight || function(){}, 
+        onSwipeDown: options.onswipedown || function(){}, 
+        onSwipeUp: options.onswipeup || function(){}, 
+    }
+
+    this.bindHandler(target);
+}
+
+Swipe.prototype.bindHandler = function(){
+    this.target.addEventListener('touchstart', this.handleTouchStart.bind(this), false);        
+    this.target.addEventListener('touchmove', this.handleTouchMove.bind(this), false);
+}
+
+Swipe.prototype.handleTouchStart = function(event) {                                         
+    this.start.x = event.touches[0].clientX;                                      
+    this.start.y = event.touches[0].clientY;                                      
+};        
+
+Swipe.prototype.handleTouchMove = function(event) {
+    if ( ! this.start.x || ! this.start.y ) {
+        return;
+    }
+
+    var gesture = {
+        x: event.touches[0].clientX,
+        y: event.touches[0].clientY
+    };
+
+    var diff = {
+        x: (this.start.x - gesture.x),
+        y: (this.start.y - gesture.y)
+    };
+
+    if ( Math.abs( diff.x ) > Math.abs( diff.y ) ) {
+        if ( diff.x > 0 ) {
+            /* left swipe */
+            this.options.onSwipeLeft();
+        } else {
+            /* right swipe */
+            this.options.onSwipeRight();
+        }                       
+    } else {
+        if ( diff.y > 0 ) {
+            /* up swipe */ 
+            this.options.onSwipeUp();
+        } else { 
+            /* down swipe */
+            this.options.onSwipeDown();
+        }                                                                 
+    }
+    /* reset values */
+    this.start.x = null;
+    this.start.y = null;                                             
+};                                       
